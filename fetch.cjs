8764b4e3d6f06c2e406816950e024fee5293e468
giry02 <158979591+giry02@users.noreply.github.com>
2026-04-13 12:42:45 +0900

    html로 수정

--- fetch.cjs
+++
@@ -1,9 +0,0 @@
-const http = require('http'); 
-http.get('http://www.dasomsnc.co.kr/html/sub_company04.html', (res) => { 
-  let html = ''; 
-  res.on('data', (c) => html += c); 
-  res.on('end', () => {
-    const matches = html.match(/<img[^>]*src=["']([^"']*)["'][^>]*>/gi);
-    console.log(matches ? matches.join('\n') : 'no images');
-  });
-});
